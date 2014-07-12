f873ed4554bdb38a3a6a907219a1a107fab02d26
L171 <L171@BHNJL171>
2014-07-12 09:56:10 +0800

    111111111111

---
+++ Career/src/com/eq/service/SpringMVCTest.java
@@ -0,0 +1,19 @@
+package com.eq.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMVCTest
+{
+	
+}
+
+
