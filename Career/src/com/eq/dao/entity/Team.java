a090b55f4d3a3e62b7d6f0b09bebb01eb537be0e
L171 <L171@BHNJL171>
2014-08-26 19:25:17 +0800

    删除bin

---
+++ Career/src/com/eq/dao/entity/Team.java
@@ -0,0 +1,56 @@
+package com.eq.dao.entity;
+
+public class Team extends BaseEntity
+{
+
+	/**
+	 * @fields serialVersionUID : TODO
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private Integer orderNo;
+	private String chn;
+	private String eng;
+	private String belong;
+	public Integer getId()
+	{
+		return id;
+	}
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
+	public String getChn()
+	{
+		return chn;
+	}
+	public void setChn(String chn)
+	{
+		this.chn = chn;
+	}
+	public String getEng()
+	{
+		return eng;
+	}
+	public void setEng(String eng)
+	{
+		this.eng = eng;
+	}
+	public String getBelong()
+	{
+		return belong;
+	}
+	public void setBelong(String belong)
+	{
+		this.belong = belong;
+	}
+	public Integer getOrderNo()
+	{
+		return orderNo;
+	}
+	public void setOrderNo(Integer orderNo)
+	{
+		this.orderNo = orderNo;
+	} 
+
+}
