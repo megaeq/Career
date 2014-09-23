38352cdbce3993baa942da0f1223c8dee356fa34
L171 <L171@BHNJL171>
2014-09-23 09:17:35 +0800

    201409230917

--- Career/src/com/test/Test.java
+++ Career/src/com/test/Test.java
@@ -1,15 +1,17 @@
 package com.test;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 public class Test {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		boolean s = true;
-		if (s) {
-
-		}
+		String jsonstring = "[{\"ebookType\":\"Z1\",\"id\":7,\"imagePath\":\"\",\"pointValue\":0,\"subType\":\"预防3\",\"title\":\"肠痉挛症表现\",\"type\":\"疾病\"},{\"ebookType\":\"Z1\",\"id\":6,\"imagePath\":\"\",\"pointValue\":0,\"subType\":\"预防3\",\"title\":\"肠痉挛症病因\",\"type\":\"疾病\"},{\"ebookType\":\"Z1\",\"id\":5,\"imagePath\":\"\",\"pointValue\":3,\"subType\":\"预防1\",\"title\":\"环境适应反应类介绍\",\"type\":\"疾病\"}]";
+		JSONArray jsonArray = JSONArray.fromObject(jsonstring);
+		System.out.println(jsonArray);
 	}
 
 }
