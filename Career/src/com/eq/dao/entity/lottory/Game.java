9cf902a730caadf93cba851271331271b62c7f56
L171 <L171@BHNJL171>
2014-11-10 16:47:04 +0800

    201411101646

--- Career/src/com/eq/dao/entity/lottory/Game.java
+++ Career/src/com/eq/dao/entity/lottory/Game.java
@@ -5,194 +5,268 @@ import java.sql.Timestamp;
 import com.eq.dao.entity.BaseEntity;
 import com.eq.util.DateUtil;
 
-public class Game extends BaseEntity {
+public class Game extends BaseEntity
+{
 
 	/**
 	 * 
 	 */
-	private static final long	serialVersionUID	= 1L;
-	private Integer				id;
-	private String				code;
-	private String				gameType;
-	private Integer				homeTeamId;
-	private Integer				lettheball;
-	private String				homeTeam;
-	private Integer				guestTeamId;
-	private String				guestTeam;
-	private Float				winRate;
-	private Float				drawRate;
-	private Float				loseRate;
-	private Integer				homeHalfScore;
-	private Integer				guestHalfScore;
-	private Integer				homeScore;
-	private Integer				guestScore;
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String code;
+	private String gameType;
+	private Integer homeTeamId;
+	private Integer lettheball;
+	private String homeTeam;
+	private Integer guestTeamId;
+	private String guestTeam;
+	private Float winRate;
+	private Float drawRate;
+	private Float loseRate;
+	private Integer homeHalfScore;
+	private Integer guestHalfScore;
+	private Integer homeScore;
+	private Integer guestScore;
 	// 天气
-	private String				weather;
+	private String weather;
 	// 310
-	private String				result;
-	private Timestamp			time;
-	private String				time2;
-	private String				suggest;
+	private String result;
+	private Timestamp time;
+	private String time2;
+	private String suggest;
+	private Double Pw;
+	private Double Pd;
+	private Double Pl;
 
-	public Integer getId() {
+	public Integer getId()
+	{
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Integer id)
+	{
 		this.id = id;
 	}
 
-	public String getCode() {
+	public String getCode()
+	{
 		return code;
 	}
 
-	public void setCode(String code) {
+	public void setCode(String code)
+	{
 		this.code = code;
 	}
 
-	public Integer getHomeTeamId() {
+	public Integer getHomeTeamId()
+	{
 		return homeTeamId;
 	}
 
-	public void setHomeTeamId(Integer homeTeamId) {
+	public void setHomeTeamId(Integer homeTeamId)
+	{
 		this.homeTeamId = homeTeamId;
 	}
 
-	public String getHomeTeam() {
+	public String getHomeTeam()
+	{
 		return homeTeam;
 	}
 
-	public void setHomeTeam(String homeTeam) {
+	public void setHomeTeam(String homeTeam)
+	{
 		this.homeTeam = homeTeam;
 	}
 
-	public Integer getGuestTeamId() {
+	public Integer getGuestTeamId()
+	{
 		return guestTeamId;
 	}
 
-	public void setGuestTeamId(Integer guestTeamId) {
+	public void setGuestTeamId(Integer guestTeamId)
+	{
 		this.guestTeamId = guestTeamId;
 	}
 
-	public String getGuestTeam() {
+	public String getGuestTeam()
+	{
 		return guestTeam;
 	}
 
-	public void setGuestTeam(String guestTeam) {
+	public void setGuestTeam(String guestTeam)
+	{
 		this.guestTeam = guestTeam;
 	}
 
-	public Float getWinRate() {
+	public Float getWinRate()
+	{
 		return winRate;
 	}
 
-	public void setWinRate(Float winRate) {
+	public void setWinRate(Float winRate)
+	{
 		this.winRate = winRate;
 	}
 
-	public Float getDrawRate() {
+	public Float getDrawRate()
+	{
 		return drawRate;
 	}
 
-	public void setDrawRate(Float drawRate) {
+	public void setDrawRate(Float drawRate)
+	{
 		this.drawRate = drawRate;
 	}
 
-	public Float getLoseRate() {
+	public Float getLoseRate()
+	{
 		return loseRate;
 	}
 
-	public void setLoseRate(Float loseRate) {
+	public void setLoseRate(Float loseRate)
+	{
 		this.loseRate = loseRate;
 	}
 
-	public Integer getHomeHalfScore() {
+	public Integer getHomeHalfScore()
+	{
 		return homeHalfScore;
 	}
 
-	public void setHomeHalfScore(Integer homeHalfScore) {
+	public void setHomeHalfScore(Integer homeHalfScore)
+	{
 		this.homeHalfScore = homeHalfScore;
 	}
 
-	public Integer getGuestHalfScore() {
+	public Integer getGuestHalfScore()
+	{
 		return guestHalfScore;
 	}
 
-	public void setGuestHalfScore(Integer guestHalfScore) {
+	public void setGuestHalfScore(Integer guestHalfScore)
+	{
 		this.guestHalfScore = guestHalfScore;
 	}
 
-	public Integer getHomeScore() {
+	public Integer getHomeScore()
+	{
 		return homeScore;
 	}
 
-	public void setHomeScore(Integer homeScore) {
+	public void setHomeScore(Integer homeScore)
+	{
 		this.homeScore = homeScore;
 	}
 
-	public Integer getGuestScore() {
+	public Integer getGuestScore()
+	{
 		return guestScore;
 	}
 
-	public void setGuestScore(Integer guestScore) {
+	public void setGuestScore(Integer guestScore)
+	{
 		this.guestScore = guestScore;
 	}
 
-	public String getSuggest() {
+	public String getSuggest()
+	{
 		return suggest;
 	}
 
-	public void setSuggest(String suggest) {
+	public void setSuggest(String suggest)
+	{
 		this.suggest = suggest;
 	}
 
-	public String getGameType() {
+	public String getGameType()
+	{
 		return gameType;
 	}
 
-	public void setGameType(String gameType) {
+	public void setGameType(String gameType)
+	{
 		this.gameType = gameType;
 	}
 
-	public String getWeather() {
+	public String getWeather()
+	{
 		return weather;
 	}
 
-	public void setWeather(String weather) {
+	public void setWeather(String weather)
+	{
 		this.weather = weather;
 	}
 
-	public String getResult() {
+	public String getResult()
+	{
 		return result;
 	}
 
-	public void setResult(String result) {
+	public void setResult(String result)
+	{
 		this.result = result;
 	}
 
-	public Timestamp getTime() {
+	public Timestamp getTime()
+	{
 		return time;
 	}
 
-	public void setTime(Timestamp time) {
+	public void setTime(Timestamp time)
+	{
 		this.time = time;
 	}
 
-	public Integer getLettheball() {
+	public Integer getLettheball()
+	{
 		return lettheball;
 	}
 
-	public void setLettheball(Integer lettheball) {
+	public void setLettheball(Integer lettheball)
+	{
 		this.lettheball = lettheball;
 	}
 
-	public String getTime2() {
+	public String getTime2()
+	{
 		time2 = DateUtil.getTimeStr(time);
 		return time2;
 	}
 
-	public void setTime2(String time2) {
+	public void setTime2(String time2)
+	{
 		this.time2 = time2;
+	}
+
+	public Double getPw()
+	{
+		return Pw;
+	}
+
+	public void setPw(Double pw)
+	{
+		Pw = pw;
+	}
+
+	public Double getPd()
+	{
+		return Pd;
+	}
+
+	public void setPd(Double pd)
+	{
+		Pd = pd;
+	}
+
+	public Double getPl()
+	{
+		return Pl;
+	}
+
+	public void setPl(Double pl)
+	{
+		Pl = pl;
 	}
 
 }
