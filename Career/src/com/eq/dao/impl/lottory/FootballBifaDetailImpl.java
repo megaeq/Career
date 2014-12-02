bc8f21f181b33b95a738feb76de46dde787166f8
L171 <L171@BHNJL171>
2014-12-02 19:34:41 +0800

    201412021934

---
+++ Career/src/com/eq/dao/impl/lottory/FootballBifaDetailImpl.java
@@ -0,0 +1,57 @@
+package com.eq.dao.impl.lottory;
+
+import java.util.List;
+import java.util.Map;
+
+import com.eq.dao.entity.lottory.FootballBifaDetail;
+import com.eq.dao.entity.myinfo.IncomeAndCost;
+import com.eq.dao.impl.BaseDao;
+import com.eq.dao.inter.AbstractDao;
+import com.eq.service.mybatis.PageParameter;
+
+public class FootballBifaDetailImpl extends BaseDao implements AbstractDao<FootballBifaDetail, Integer>
+{
+
+	@Override
+	public int add(FootballBifaDetail entity)
+	{
+		return getSqlSessionTemplate().insert("footballbifadetail.add", entity);
+	}
+
+	@Override
+	public int delete(Integer id)
+	{
+		return getSqlSessionTemplate().delete("footballbifadetail.delete", id);
+	}
+
+	@Override
+	public int update(FootballBifaDetail entity)
+	{
+		return getSqlSessionTemplate().update("footballbifadetail.update", entity);
+	}
+
+	@Override
+	public Map<String, Object> selectPageList(Map<String, Object> params, int currentPage, int pageSize)
+	{
+		PageParameter pageParameter = new PageParameter(currentPage,pageSize);
+		params.put("page", pageParameter);
+		List<IncomeAndCost> list = getSqlSessionTemplate().selectList("footballbifadetail.selectPageList", params);
+		params.clear();
+		params.put("list", list);
+		params.put("count", pageParameter.getTotalCount());
+		return params;
+	}
+
+	@Override
+	public List<FootballBifaDetail> selectList(Map<String, Object> params)
+	{
+		return getSqlSessionTemplate().selectList("footballbifadetail.selectPageList", params);
+	}
+
+	@Override
+	public FootballBifaDetail selectOne(Integer id)
+	{
+		return getSqlSessionTemplate().selectOne("footballbifadetail.selectone",id);
+	}
+
+}
---
+++ Career/src/com/eq/dao/impl/lottory/FootballBifaImpl.java
@@ -0,0 +1,57 @@
+package com.eq.dao.impl.lottory;
+
+import java.util.List;
+import java.util.Map;
+
+import com.eq.dao.entity.lottory.FootballBifa;
+import com.eq.dao.entity.myinfo.IncomeAndCost;
+import com.eq.dao.impl.BaseDao;
+import com.eq.dao.inter.AbstractDao;
+import com.eq.service.mybatis.PageParameter;
+
+public class FootballBifaImpl extends BaseDao implements AbstractDao<FootballBifa, Integer>
+{
+
+	@Override
+	public int add(FootballBifa entity)
+	{
+		return getSqlSessionTemplate().insert("footballbifa.add", entity);
+	}
+
+	@Override
+	public int delete(Integer id)
+	{
+		return getSqlSessionTemplate().delete("footballbifa.delete", id);
+	}
+
+	@Override
+	public int update(FootballBifa entity)
+	{
+		return getSqlSessionTemplate().update("footballbifa.update", entity);
+	}
+
+	@Override
+	public Map<String, Object> selectPageList(Map<String, Object> params, int currentPage, int pageSize)
+	{
+		PageParameter pageParameter = new PageParameter(currentPage,pageSize);
+		params.put("page", pageParameter);
+		List<IncomeAndCost> list = getSqlSessionTemplate().selectList("footballbifa.selectPageList", params);
+		params.clear();
+		params.put("list", list);
+		params.put("count", pageParameter.getTotalCount());
+		return params;
+	}
+
+	@Override
+	public List<FootballBifa> selectList(Map<String, Object> params)
+	{
+		return getSqlSessionTemplate().selectList("footballbifa.selectPageList", params);
+	}
+
+	@Override
+	public FootballBifa selectOne(Integer id)
+	{
+		return getSqlSessionTemplate().selectOne("footballbifa.selectone", id);
+	}
+
+}
