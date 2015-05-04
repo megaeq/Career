ff5f048bdcef5de4e57a68764b37e7975ebe36ff
L171 <L171@172.16.1.156>
2015-05-04 17:56:44 +0800

    201505041756 数学模型

---
+++ Career/src/com/eq/util/mathModel/MathModel.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright(C) 2011-2012 BillionHealth Software Technology LTD. All Rights Reserved.
+ * @title MathModel.java
+ * @package com.eq.util.mathModel
+ * @compiler JDK1.6
+ * @description TODO
+ * @author Mega.Yan
+ * @date 2015-5-4 上午9:03:11
+ * @version V1.0  
+ */
+package com.eq.util.mathModel;
+
+/**
+ * @className MathModel
+ * @description TODO
+ * @author Mega.Yan
+ * @date 2015-5-4 上午9:03:11
+ */
+public interface MathModel<T>
+{
+	public Float getResult(T t);
+	
+}
