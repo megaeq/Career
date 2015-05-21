b9ff001a072f0b05aea1015f6cb6362bf9033858
L171 <L171@172.16.1.159>
2015-05-21 16:32:15 +0800

    201505211632

---
+++ Career/src/com/eq/util/IK/IKMathFunctions.java
@@ -0,0 +1,8 @@
+package com.eq.util.IK;
+
+public class IKMathFunctions
+{
+	public Double sqrt(Double d) {
+		return Math.sqrt(d);
+	}
+}
---
+++ Career/src/com/test/IK/Test.java
@@ -0,0 +1,26 @@
+package com.test.IK;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.wltea.expression.ExpressionEvaluator;
+import org.wltea.expression.datameta.Variable;
+import org.wltea.expression.function.FunctionLoader;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("(1+2)*3="+ExpressionEvaluator.evaluate("(1+2)*3"));
+		System.out.println("(1+2)*3/2.3="+ExpressionEvaluator.evaluate("(1+2)*3/2.3"));
+		Collection<Variable> col = new ArrayList<Variable>();
+		col.add(Variable.createVariable("b", 2));
+		col.add(Variable.createVariable("a", 1));
+		col.add(Variable.createVariable("c", 3));
+		System.out.println("(1+2)*3="+ExpressionEvaluator.evaluate("(a+b)*c",col));
+		System.out.println("(1+2)*3的逆波兰表达式为："+ExpressionEvaluator.compile("(a+b)*c",col));
+		System.out.println("(1+2)*3的逆波兰表达式为："+ExpressionEvaluator.compile("(1+2)*3"));
+		System.out.println("系统当前日期："+ExpressionEvaluator.evaluate("$SYSDATE()"));
+		System.out.println("根号（4）="+ExpressionEvaluator.evaluate("$开方((2d+2)*3)"));
+	}
+}
