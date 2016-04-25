c39373899dbe9842e0938ea7df0ba100e754c87a
wisdom <wisdom@172.16.1.109>
2016-04-25 09:08:45 +0800

    201604250908

---
+++ Career/src/com/sloution/Sloution15/Sloution15.java
@@ -0,0 +1,19 @@
+package com.sloution.Sloution15;
+
+public class Sloution15
+{
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 *     int val;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode(int x) { val = x; }
+	 * }
+	 */
+	public class Solution {
+	    public int rob(TreeNode root) {
+	        
+	    }
+	}
+}
